07099bb518f63eb4ea6fc6176218a1cde8fd3f6c
Miguel_fr3 <rm99997@fiap.com.br>
2023-10-25 23:31:52 -0300

    Finalizando estrutura do cabeçalho

--- src/app/components/Cabecalho/Cabecalho.jsx
+++ src/app/components/Cabecalho/Cabecalho.jsx
@@ -1,12 +1,27 @@
 import Image from 'next/image'
-
+import Link from 'next/link'
 export default function Cabecalho() {
   return (
     <header>
         <div>
-            <Image src={""}/>
-
+            <Image src="/Logo.png" alt="Logo Porto" width={200} height={200}/>
         </div>
+        <nav>
+          <ul>
+            <li>
+              <Link href="#">Home</Link>
+            </li>
+            <li>
+              <Link href="#">Ajuda</Link>
+            </li>
+            <li>
+              <Link href="#">Modals</Link>
+            </li>
+            <li>
+              <Link href="#">Sobre nós</Link>
+            </li>
+          </ul>
+        </nav>
     </header>
   )
 }
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-
+import Cabecalho from './components/Cabecalho/Cabecalho'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,7 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Cabecalho/>
+        {children}
+        </body>
     </html>
   )
 }
