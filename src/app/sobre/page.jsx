f8a3c8efb75d005fbe7201df51712b9fe04e0bdf
matheusrcdrigues <matheusrcdrigues@gmail.com>
2023-10-31 18:43:36 -0300

    iniciando pagina sobre

--- src/app/sobre/page.jsx
+++ src/app/sobre/page.jsx
@@ -1,8 +1,27 @@
 
 export default function Sobre() {
   return (
-    <div>
-        <a href="#">page sobre</a>
-    </div>
+    <main className="sobre">
+            <div>
+                <h1 className="proposito">Nosso propósito é</h1>
+                <h1 className="porto">Ser um porto seguro para as pessoas</h1>
+            </div>
+            <div className="inferior">
+            <div className="colab">
+                <h1>Colaboradores</h1>
+                <h2>rm99977 - Alberto Seiji <br />
+
+                rm551997 - Matheus Rodrigues <br />
+
+                rm99997 - Miguel Fernandes <br />
+
+                rm551521 - Patrick Jaguski <br />
+
+                rm552213 - Ye</h2> <br />
+
+                </div>
+                
+                </div>
+        </main>
   )
 }
