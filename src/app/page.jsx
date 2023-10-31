28919fd91bfdd7239a2302d0a2bce3ce79cbc8ff
patrickjaguski <patrick_jaguski@hotmail.com>
2023-10-31 17:51:52 -0300

    iniciando Home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,18 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <>
-    <a href="#">aaaaa</a>
+    <section>
+        <div className="superior">
+          <div>
+            <h1>Guincho 24 horas</h1>
+            <p>
+              Guinchos ilimitados, sem limite de quilometragem. <br />
+              Reparos do carro Reparo rápido e Supermartelinho de Ouro.
+            </p>
+          </div>
+          
+        </div>
+      </section>
     </>
 
   )
