840db3404173e92d92aeb23df1e3b6dd3130c567
luigi138 <nimapi20041012@gmail.com>
2023-11-06 13:34:54 -0300

    iniciando pag ajuda

--- src/app/ajuda/page.jsx
+++ src/app/ajuda/page.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCog, faContactBook, faExclamationTriangle, faMobile, faMoneyBill1Wave, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 
 export default function Ajuda() {
   return (
-    <div>
-        <a href="#">page</a>
-    </div>
+<main className="ajuda">
+
+            <div className="help">
+            <label htmlFor="ajuda">Como podemos te ajudar?</label>
+            <input type="text" id="ajuda" placeholder="Pesquise o que você precisa aqui!" />
+            </div>
+            <div className="superior">
+                <div><FontAwesomeIcon icon={faExclamationTriangle} className='icona'/><h1>Solicitar Socorro</h1></div>
+                <div><FontAwesomeIcon icon={faContactBook} className='icona'/><h1>Contatos</h1></div>
+                <div><FontAwesomeIcon icon={faCog} className='icona'/><h1>Editar dados</h1></div>
+            </div>
+
+        </main>
   )
 }
