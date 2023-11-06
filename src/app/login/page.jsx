48e0dfacdadd4b5ebfffcdf1801afd8d2bc71f6c
luigi138 <nimapi20041012@gmail.com>
2023-11-06 13:10:03 -0300

    iniciando pagina login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 export default function Login() {
   return (
-    <div>
-      <p>login</p>
-    </div>
+    <main className="login">
+      <div className="form">
+        <form className="formulario">
+          <Link href="/"><FontAwesomeIcon icon={faArrowLeft} className='flecha' /></Link>
+          <h1>Acessar Conta</h1>
+          <label htmlFor="Cpf">Preencha seus dados de acesso para continuar.</label>
+          <input type="text" id="Cpf" placeholder="CPF ou CNPJ" />
+          <div className="register">
+            <input type="submit" id="logar" value="Entrar" />
+          </div>
+          <div className="check">
+            <input type="checkbox" id="check" /> 
+            <label htmlFor="check">Lembre de mim</label> 
+          </div>
+          <Link href="#">Sou novo aqui</Link>
+        </form>
+      </div>
+    </main>
   )
 }
