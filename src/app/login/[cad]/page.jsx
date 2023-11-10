b9352d715167c9d2062a45be3f9673e73b114632
Miguel_Fr3 <miguel.fernandes2004f2@gmail.com>
2023-11-10 20:17:35 -0300

    Criando cadastro

--- src/app/login/[cad]/page.jsx
+++ src/app/login/[cad]/page.jsx
@@ -1,9 +1,27 @@
-
+import React from 'react'
+import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import "../login.scss"
 
 export default function Cadastro() {
   return (
-    <div>
-      cadastro
+    <main className="login">
+    <div className="form">
+    <form className="formulario">
+    <Link href="/login"><FontAwesomeIcon icon={faArrowLeft} className='flecha' /></Link>
+        <h1>Cadastrar Conta</h1>
+        <label htmlFor="Cpf">Preencha seus dados de acesso para continuar.</label>
+        <input type="text" id="Cpf" placeholder="CPF ou CNPJ"/>
+        <label htmlFor="Senha"></label>
+          <input type="password" id="Senha" placeholder="Senha" />
+        <div className="register">
+            <input type="submit" value="Cadastrar"/>
+        </div>
+
+    </form>
     </div>
+</main>
   )
 }
+
