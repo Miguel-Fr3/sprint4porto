59c205571c6bd7ac81409016f61b974685fd596d
Miguel-fr3 <miguel.fernandes2004f2@gmail.com>
2023-11-14 22:55:02 -0300

    testando correção de erro

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -41,13 +41,13 @@ const handleSubmit = async (e)=>{
   e.preventDefault();
 
   try {
-    const response = await fetch("http://localhost:8080/api/login/autenticar",{
-        method: "POST",
-        headers:{
-            "Content-Type":"application/json"
-        },
-        body:  JSON.stringify(usuario)
-    });
+    // const response = await fetch("http://localhost:8080/api/login/autenticar",{
+    //     method: "POST",
+    //     headers:{
+    //         "Content-Type":"application/json"
+    //     },
+    //     body:  JSON.stringify(usuario)
+    // });
 
     if(response.ok){
         const user = await response.json();
